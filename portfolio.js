623ada5f44af3f2565e56b26404042d6f572fd68
B Salinas <b.salinas397@gmail.com>
2025-06-09 11:50:56 -0500

    updated to more specific import mapping

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from 'https://unpkg.com/three@0.156.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.156.1/examples/jsm/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
