d122b10ad9a9d87e3d6fa8922641c2291a93e7e0
B Salinas <b.salinas397@gmail.com>
2025-06-09 08:45:03 -0500

    Update portfolio.js
    put it back to use importmaps

--- portfolio.js
+++ portfolio.js
@@ -2,8 +2,8 @@
 // cursor.js
 
 // You'll need to use CDN imports instead of npm-style imports
-import * as THREE from 'https://cdn.skypack.dev/three@0.132.2';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Rest of your code stays the same
 
