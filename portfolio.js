b109e04a5e300698e49cdfb7dbaa97ff40642ec3
B Salinas <b.salinas397@gmail.com>
2025-06-17 19:50:06 -0500

    hopefully i fixed uncaught typeerror

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@
 // cursor.js
 
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // Add floating text overlay
 const instructionText = document.createElement('div');
