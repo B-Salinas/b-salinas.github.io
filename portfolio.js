a363711a14d33a7e4aeb7abccc6e57489e8721b9
B <b.salinas397@gmail.com>
2025-10-02 22:33:13 -0500

    Update portfolio.js
    -.-

--- portfolio.js
+++ portfolio.js
@@ -5,6 +5,7 @@ import * as THREE from 'https://unpkg.com/three@0.156.1/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.156.1/examples/jsm/controls/OrbitControls.js';
 import { ArcballControls } from 'https://unpkg.com/three@0.156.1/examples/jsm/controls/ArcballControls.js';
 
+
 // Add floating text overlay
 const instructionText = document.createElement('div');
 instructionText.innerHTML = 'Use mouse to move around • Drag to rotate • Scroll to zoom';
